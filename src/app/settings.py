934985db3b43f5b3de379011dc0f58670de48bf4
zander404 <alexandrearthurpg@gmail.com>
2023-08-01 10:51:41 -0300

    config setting for containerize the image

--- src/app/settings.py
+++ src/app/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
@@ -155,7 +156,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://BackEnd_Nolex:8000',
     'http://NGINX_Nolex',
-    'nolex.xandyy.dev',
+    'http://nolex.xandyy.dev',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
