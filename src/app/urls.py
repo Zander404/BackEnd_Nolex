5ca2283b9b152d22f85b3ef225ca70b027be8af7
zander404 <alexandrearthurpg@gmail.com>
2023-08-03 11:41:18 -0300

    test

--- src/app/urls.py
+++ src/app/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('blog/', include('blog.urls')),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    
 ]
